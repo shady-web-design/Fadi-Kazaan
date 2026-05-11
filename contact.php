be7a0510864eceac705a3ce7c7218827643c9c6a
Shood99j <21imc10970@fh-krems.ac.at>
2026-05-11 23:57:54 +0200

    First time upload

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "your-email@example.com";
+    $subject = "New message from Fadi Kazaan website";
+
+    $body = "Name: " . $name . "\n";
+    $body .= "Email: " . $email . "\n\n";
+    $body .= "Message:\n" . $message;
+
+    $headers = "From: " . $email;
+
+    if (mail(saljibaee@gmail.com, $subject, $body, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Message could not be sent.";
+    }
+}
+?>
